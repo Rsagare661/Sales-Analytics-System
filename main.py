cb41055b5b693faf2b174bfb270774ad7b4d3e42
Raj Sagare <Rajsagare2002@gmail.com>
2026-07-01 19:52:59 +0530

    Display sales records in readable format

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import csv
 
+print("=" * 50)
+print("     SALES ANALYTICS SYSTEM")
+print("=" * 50)
+
 file = open("Data/sales.csv", "r")
+
 reader =csv.reader(file)
+next(reader)
 
 for row in reader:
-    print(row)
+    print("=" * 50)
+
+    print("order Id      :", row[0])
+    print("Date          :", row[1])
+    print("Customer Name :", row[2])
+    print("Product       :", row[3])
+    print("Category      :", row[4])
+    print("Quantity      :", row[5])
+    print("Price         :", row[6])
 
 file.close()
