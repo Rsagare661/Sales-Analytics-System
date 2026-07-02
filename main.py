050e84d03bc15e43ced69a01e62b7e4003e79222
Raj Sagare <Rajsagare2002@gmail.com>
2026-07-02 19:04:28 +0530

    Complete revenue calculation feature

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 import csv
 
-print("=" * 50)
-print("     SALES ANALYTICS SYSTEM")
-print("=" * 50)
+def display_title():
+    print("=" * 50)
+    print("     SALES ANALYTICS SYSTEM")
+    print("=" * 50)
+
+display_title()
 
 file = open("Data/sales.csv", "r")
 
 reader =csv.reader(file)
 next(reader)
 
-for row in reader:
-    print("=" * 50)
+total_revenue = 0 
 
-    print("order Id      :", row[0])
+for row in reader:
+
+    quantity = int(row[5])
+    price = int(row[6])
+    revenue = quantity * price
+
+    total_revenue += revenue
+    
+    print("=" * 50)
+    print("Order ID      :", row[0])
     print("Date          :", row[1])
     print("Customer Name :", row[2])
     print("Product       :", row[3])
     print("Category      :", row[4])
     print("Quantity      :", row[5])
-    print("Price         :", row[6])
+    print("Price         : ₹", row[6])
+    print("Revenue       : ₹", revenue)
+    
+print("=" * 50)
+print("Total Revenue : ₹", total_revenue)
 
 file.close()
